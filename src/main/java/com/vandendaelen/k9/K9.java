f0a4fd897e032bac608e132b482fd2e1384ccabc
LotuxPunk <clement@vandendaelen.com>
2018-06-08 20:25:05 +0200

    remove testing code & fix bug

--- src/main/java/com/vandendaelen/k9/K9.java
+++ src/main/java/com/vandendaelen/k9/K9.java
@@ -1,36 +1,35 @@
 package com.vandendaelen.k9;
 
-import com.vandendaelen.k9.event.RegisteringEvent;
 import com.vandendaelen.k9.proxy.CommonProxy;
 import com.vandendaelen.k9.utils.Reference;
-import net.minecraftforge.common.MinecraftForge;
+import com.vandendaelen.k9.utils.handlers.RegistryHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MODID, name =Reference.NAME , version =Reference.VERSION )
 public class K9 {
     @Mod.Instance(Reference.MODID)
     public static K9 instance;
 
-    public K9() {
-        MinecraftForge.EVENT_BUS.register(new RegisteringEvent());
-    }
-
     @SidedProxy(clientSide = Reference.CLIENT_PROXY,serverSide = Reference.SERVER_PROXY)
     public static CommonProxy proxy;
 
     @Mod.EventHandler
-    public void preInit(){
+    public void preInit(FMLPreInitializationEvent e){
         proxy.preInit();
+        RegistryHandler.preInitRegistries();
     }
 
     @Mod.EventHandler
-    public void init(){
+    public void init(FMLInitializationEvent e){
         proxy.init();
     }
 
     @Mod.EventHandler
-    public void postInit(){
+    public void postInit(FMLPostInitializationEvent e){
         proxy.postInit();
     }
 }
---
+++ src/main/java/com/vandendaelen/k9/utils/handlers/RegistryHandler.java
@@ -0,0 +1,21 @@
+package com.vandendaelen.k9.utils.handlers;
+
+import com.vandendaelen.k9.init.ModEntity;
+
+public class RegistryHandler {
+//    @SubscribeEvent
+//    public void onItemRegister(RegistryEvent.Register<Item> e){
+//
+//    }
+//
+//    @SubscribeEvent
+//    public void onBlockRegister(RegistryEvent.Register<Block> e){
+//
+//    }
+
+    public static void preInitRegistries(){
+        ModEntity.registerEntities();
+
+        RenderHandler.registerEntityRenders();
+    }
+}
