826450f5e0e0630a11e2356854f0e915b0e103b5
LotuxPunk <clement@vandendaelen.com>
2018-07-02 12:20:22 +0200

    version bump

--- src/main/java/com/vandendaelen/k9/utils/Reference.java
+++ src/main/java/com/vandendaelen/k9/utils/Reference.java
@@ -3,7 +3,7 @@ package com.vandendaelen.k9.utils;
 public class Reference {
     public static final String MODID ="k9";
     public static final String NAME = "K9 Mod";
-    public static final String VERSION = "0.7";
+    public static final String VERSION = "0.7.1";
     public static final String DEPENDENCIES = "required:forge@[14.23.4.2725,)";
 
     public static final String CLIENT_PROXY = "com.vandendaelen.k9.proxy.ClientProxy";
