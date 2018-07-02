83930ceaef1612c93a901e1dc7e04a731dcd1258
LotuxPunk <clement@vandendaelen.com>
2018-07-02 14:27:03 +0200

    fix crash at launch

--- src/main/java/com/vandendaelen/k9/utils/Reference.java
+++ src/main/java/com/vandendaelen/k9/utils/Reference.java
@@ -3,7 +3,7 @@ package com.vandendaelen.k9.utils;
 public class Reference {
     public static final String MODID ="k9";
     public static final String NAME = "K9 Mod";
-    public static final String VERSION = "0.7.1";
+    public static final String VERSION = "0.7.2";
     public static final String DEPENDENCIES = "required:forge@[14.23.4.2725,)";
 
     public static final String UPDATE_JSON = "https://raw.githubusercontent.com/LotuxPunk/K9/master/update.json";
