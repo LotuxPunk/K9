224614657abb3720ed9ba17517241f90fdb432f6
LotuxPunk <clement@vandendaelen.com>
2018-08-04 23:35:21 +0200

    useless method

--- src/main/java/com/vandendaelen/k9/utils/Utils.java
+++ src/main/java/com/vandendaelen/k9/utils/Utils.java
@@ -1,27 +1,14 @@
 package com.vandendaelen.k9.utils;
 
-import com.vandendaelen.k9.entities.EntityK9;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
 
-import java.util.List;
 import java.util.UUID;
 
 public class Utils {
 
     public static EntityPlayer getPlayer (UUID id){
         return Minecraft.getMinecraft().world.getPlayerEntityByUUID(id);
-    }
-
-    public static EntityK9 isAClickOnK9(EntityPlayer player, BlockPos pos){
-        List<Entity> entities = Minecraft.getMinecraft().world.getLoadedEntityList();
-        for (Entity entity : entities) {
-            if (entity.getPosition() == pos && entity instanceof EntityK9)
-                return (EntityK9)entity;
-        }
-        return null;
     }
 
 }
