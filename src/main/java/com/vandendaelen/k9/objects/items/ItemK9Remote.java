62c17c8563d5ef79e4d2ea5f1441167faaaee56a
LotuxPunk <clement@vandendaelen.com>
2018-08-05 11:06:20 +0200

    clean import and debug message

--- src/main/java/com/vandendaelen/k9/objects/items/ItemK9Remote.java
+++ src/main/java/com/vandendaelen/k9/objects/items/ItemK9Remote.java
@@ -1,21 +1,13 @@
 package com.vandendaelen.k9.objects.items;
 
 import com.vandendaelen.k9.entities.EntityK9;
-import com.vandendaelen.k9.utils.Utils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.Sys;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -32,28 +24,15 @@ public class ItemK9Remote extends ItemBase {
     public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
         if (!target.world.isRemote && target instanceof EntityK9){
             this.setK9ID(stack, target.getUniqueID());
-            System.out.println(getK9ID(stack).toString());
             return true;
         }
         return super.itemInteractionForEntity(stack, playerIn, target, hand);
     }
 
-    /*@Override
-    public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
-        if(!target.world.isRemote){
-            if (target instanceof EntityK9){
-                this.setK9ID(stack, target.getUniqueID());
-                return true;
-            }
-        }
-        return super.itemInteractionForEntity(stack, playerIn, target, hand);
-    }*/
-
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
         if(stack.hasTagCompound() && stack.getTagCompound().hasUniqueId(NBT.K9ID)) {
             tooltip.add("ID K9 " + this.getK9ID(stack).toString());
-            System.out.println("Ca passe");
         }
         else {
             tooltip.add("You should be in survival mod to set the link between K9 and this remote");
