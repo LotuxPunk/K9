73f163c261f74d91c15c8a483ee305c5ed3e9098
LotuxPunk <clement@vandendaelen.com>
2018-08-05 17:55:36 +0200

    K9 teleporting when click on remote

--- src/main/java/com/vandendaelen/k9/objects/items/ItemK9Remote.java
+++ src/main/java/com/vandendaelen/k9/objects/items/ItemK9Remote.java
@@ -1,12 +1,18 @@
 package com.vandendaelen.k9.objects.items;
 
 import com.vandendaelen.k9.entities.EntityK9;
+import com.vandendaelen.k9.utils.K9Teleporter;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -14,6 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 public class ItemK9Remote extends ItemBase {
+
 
     public ItemK9Remote(String name) {
         super(name);
@@ -23,10 +30,30 @@ public class ItemK9Remote extends ItemBase {
     @Override
     public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target, EnumHand hand) {
         if (!target.world.isRemote && target instanceof EntityK9){
-            this.setK9ID(stack, target.getUniqueID());
+            this.setK9ID(stack, target.getPersistentID());
             return true;
         }
         return super.itemInteractionForEntity(stack, playerIn, target, hand);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        if (!worldIn.isRemote){
+            UUID uuid = getK9ID(playerIn.getHeldItem(handIn));
+            if (uuid != null){
+                EntityK9 k9 = (EntityK9)worldIn.getMinecraftServer().getEntityFromUuid(uuid);
+                if (k9 != null && k9.getOwnerId().equals(playerIn.getUniqueID())){
+                    if (k9.dimension != playerIn.dimension) K9Teleporter.teleportDimEntity(k9,playerIn.getPosition(),playerIn.dimension);
+
+                    k9.setLocationAndAngles(playerIn.posX,playerIn.posY,playerIn.posZ, playerIn.cameraYaw,playerIn.cameraPitch);
+
+                    return ActionResult.newResult(EnumActionResult.SUCCESS,playerIn.getHeldItem(handIn));
+                }
+                return ActionResult.newResult(EnumActionResult.PASS,playerIn.getHeldItem(handIn));
+            }
+            return ActionResult.newResult(EnumActionResult.PASS,playerIn.getHeldItem(handIn));
+        }
+        return ActionResult.newResult(EnumActionResult.PASS,playerIn.getHeldItem(handIn));
     }
 
     @Override
---
+++ src/main/java/com/vandendaelen/k9/utils/K9Teleporter.java
@@ -0,0 +1,67 @@
+package com.vandendaelen.k9.utils;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Teleporter;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+
+public class K9Teleporter extends Teleporter {
+
+    public K9Teleporter(WorldServer worldIn) {
+        super(worldIn);
+    }
+
+    public static boolean teleportDimEntity(Entity entity, BlockPos pos, int targetDim) {
+        if (entity.getEntityWorld().isRemote || entity.isRiding() || entity.isBeingRidden() || !entity.isEntityAlive()) {
+            return false;
+        }
+        int from = entity.dimension;
+        if (from != targetDim) {
+            MinecraftServer server = entity.getServer();
+            WorldServer fromDim = server.getWorld(from);
+            WorldServer toDim = server.getWorld(targetDim);
+            Teleporter teleporter = new K9Teleporter(toDim);
+
+            NBTTagCompound tagCompound = entity.serializeNBT();
+            float rotationYaw = entity.rotationYaw;
+            float rotationPitch = entity.rotationPitch;
+            fromDim.removeEntity(entity);
+            Entity newEntity = EntityList.createEntityFromNBT(tagCompound, toDim);
+
+            if (newEntity != null) {
+                newEntity.setLocationAndAngles(pos.getX(), pos.getY(), pos.getZ(), rotationYaw, rotationPitch);
+                newEntity.forceSpawn = true;
+                toDim.spawnEntity(newEntity);
+                newEntity.forceSpawn = false;
+            } else {
+                return false;
+            }
+        }
+        if (!entity.world.isBlockLoaded(pos)) {
+            entity.world.getChunkFromBlockCoords(pos);
+        }
+        entity.setPositionAndUpdate(pos.getX() + 0.5D, pos.getY() + 0.1D, pos.getZ() + 0.5D);
+        entity.fallDistance = 0;
+        return true;
+    }
+
+    @Override
+    public void placeInPortal(Entity entityIn, float rotationYaw) {}
+
+    @Override
+    public boolean placeInExistingPortal(Entity entityIn, float rotationYaw) {
+        return true;
+    }
+
+    @Override
+    public boolean makePortal(Entity entityIn) {
+        return true;
+    }
+
+    @Override
+    public void removeStalePortalLocations(long worldTime) {}
+}
