9e507e47ea39308b177501113c8a365d7b72bd21
LotuxPunk <clement@vandendaelen.com>
2018-08-20 13:40:29 +0200

    One entity/ 2 models

--- src/main/java/com/vandendaelen/k9/entities/render/RenderK9.java
+++ src/main/java/com/vandendaelen/k9/entities/render/RenderK9.java
@@ -2,28 +2,67 @@ package com.vandendaelen.k9.entities.render;
 
 import com.vandendaelen.k9.entities.EntityK9;
 import com.vandendaelen.k9.entities.model.ModelK9;
+import com.vandendaelen.k9.init.K9Items;
 import com.vandendaelen.k9.utils.Reference;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nullable;
 
 public class RenderK9 extends RenderLiving<EntityK9> {
 
-    public static final ResourceLocation TEXTURES = new ResourceLocation(Reference.MODID+":textures/entity/k9.png");
+    private final ResourceLocation TEXTURES_OLD = new ResourceLocation(Reference.MODID+":textures/entity/k9.png");
+
+    private ModelBase modelK9 = new ModelK9();
+    private Item item = K9Items.dummy_k9;
+
     public RenderK9(RenderManager manager) {
         super(manager, new ModelK9(), 0.5F);
-    }
-
-    @Nullable
-    @Override
-    protected ResourceLocation getEntityTexture(EntityK9 entity) {
-        return TEXTURES;
+        mainModel = modelK9;
     }
 
     @Override
     protected void applyRotations(EntityK9 entityLiving, float p_77043_2_, float rotationYaw, float partialTicks) {
         super.applyRotations(entityLiving, p_77043_2_, rotationYaw, partialTicks);
+    }
+
+    @Override
+    public void doRender(EntityK9 entity, double x, double y, double z, float entityYaw, float partialTicks) {
+        if (entity.isMarkII()){
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(x, y + 0.4, z);
+            float f7 = entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks;
+            float f8 = this.handleRotationFloat(entity, partialTicks);
+            float f = this.interpolateRotation(entity.prevRenderYawOffset, entity.renderYawOffset, partialTicks);
+            this.applyRotations(entity, f8, f, partialTicks);
+            this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+            Minecraft.getMinecraft().getRenderItem().renderItem(getStackToRender(), ItemCameraTransforms.TransformType.GROUND);
+            GlStateManager.popMatrix();
+        }
+        else {
+            super.doRender(entity,x,y,z,entityYaw,partialTicks);
+        }
+
+    }
+
+
+    private ItemStack getStackToRender() {
+        return new ItemStack(this.item);
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(EntityK9 entity) {
+        if (entity.isMarkII()){
+            return TextureMap.LOCATION_BLOCKS_TEXTURE;
+        }
+        return TEXTURES_OLD;
     }
 }
--- src/main/java/com/vandendaelen/k9/utils/handlers/RenderHandler.java
+++ src/main/java/com/vandendaelen/k9/utils/handlers/RenderHandler.java
@@ -2,14 +2,14 @@ package com.vandendaelen.k9.utils.handlers;
 
 import com.vandendaelen.k9.entities.EntityK9;
 import com.vandendaelen.k9.entities.EntityK9Ray;
-import com.vandendaelen.k9.entities.render.RenderK9New;
+import com.vandendaelen.k9.entities.render.RenderK9;
 import com.vandendaelen.k9.entities.render.RenderK9Ray;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class RenderHandler {
 
     public static void registerEntityRenders(){
-        RenderingRegistry.registerEntityRenderingHandler(EntityK9.class, RenderK9New::new);
+        RenderingRegistry.registerEntityRenderingHandler(EntityK9.class, RenderK9::new);
         RenderingRegistry.registerEntityRenderingHandler(EntityK9Ray.class, RenderK9Ray::new);
     }
 }
